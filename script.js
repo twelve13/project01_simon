2282836e6305d827dcb662077e73e60366784703
Caroline Chen <carolinecaroline@gmail.com>
2017-04-03 13:34:07 -0400

    start on game logic

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ var yellowButton = $("#yellow");
 var blueButton = $("#blue");
 var startButton = $("#start");
 
+var choice;
+
 var greenLight = function() {
 	greenButton.css("background-color", "limegreen");
 }
+var greenOff = function(){
+	setTimeout(greenLight(), 3000)}
 
 var redLight = function(){
 	redButton.css("background-color", "#ff170f");
@@ -29,16 +33,56 @@ var blueLight = function(){
 var targetSequence = [];
 
 var getNextColor = function(){
-	console.log("next is ")
-	targetSequence.push(Math.floor((Math.random() * 4 +1)));
-	console.log(targetSequence);
+	targetSequence.push(Math.floor((Math.random() * 4 +1)));	
 }
 
 var play = function(){
-	for (var i = 0; i < 8; i++){
+	for (var i = 0; i < 1; i++){
+	console.log("next is ")
 	getNextColor();
+	console.log(targetSequence);
+	if (targetSequence[i] == 1){
+		console.log("one was chosen");
+		greenLight();
+		greenOff();
+		// setTimeout(greenLight(), 3000);
+	} else if (targetSequence[i] == 2){
+		console.log("two was chosen");
+		redLight();
+	} else if (targetSequence[i] == 3){
+		console.log("three was chosen");
+		yellowLight();
+	} else if (targetSequence[i] == 4){
+		console.log("four was chosen");
+		blueLight();
 	}
+	}
+
 }
 
+var selectGreen = function(){
+	console.log("clicked on green");
+	return choice = 1;
+}
+
+var selectRed = function(){
+	console.log("clicked on red");
+	return choice = 2;
+}
+
+var selectYellow = function(){
+	console.log("clicked on yellow");
+	return choice = 3;
+}
+
+var selectBlue = function(){
+	console.log("clicked on blue");
+	return choice = 4;
+}
 
 startButton.on("click", play);
+
+greenButton.on("click", selectGreen);
+redButton.on("click", selectRed);
+yellowButton.on("click", selectYellow);
+blueButton.on("click", selectBlue);
