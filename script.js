451a19fecb421ef20215165e0859b766b3c10998
Caroline Chen <carolinecaroline@gmail.com>
2017-04-04 14:31:17 -0400

    add time delay between light flashes

--- script.js
+++ script.js
@@ -12,31 +12,39 @@ var startButton = $("#start");
 
 var greenLight = function() {
 	greenButton.css("background-color", "limegreen");
-}
-var greenOff = function(){
-	greenButton.css("background-color", "green");
-}
+	var greenOff = function(){
+		greenButton.css("background-color", "green");
+	}
+	setTimeout(greenOff, 500);
+};
+
 
 var redLight = function(){
 	redButton.css("background-color", "#ff170f");
-}
-var redOff = function(){
-	redButton.css("background-color", "#a8000b");
-}
+	var redOff = function(){
+		redButton.css("background-color", "#a8000b");
+	}
+	setTimeout(redOff, 500);
+};
+
 
 var yellowLight = function(){
 	yellowButton.css("background-color", "yellow");
-}
-var yellowOff = function(){
-	yellowButton.css("background-color", "#d6c100");
-}
+	var yellowOff = function(){
+		yellowButton.css("background-color", "#d6c100");
+	}
+	setTimeout(yellowOff, 500);
+};
+
 
 var blueLight = function(){
-	blueButton.css("background-color", "#1c45fd");
-}
-var blueOff = function(){
-	blueButton.css("background-color", "#2034b6");
-}
+	blueButton.css("background-color", "#1c58fd");
+	var blueOff = function(){
+		blueButton.css("background-color", "#2c42af");
+	}
+	setTimeout(blueOff, 500);
+};
+
 
 
 var targetSequence = [];
@@ -45,75 +53,40 @@ var inputSequence = [];
 var inPlay = true;
 
 var compare = function(){
-	// if (targetSequence.length !== inputSequence.length) return false;
-	for (var i = 0; i < targetSequence.length; i++){	
+	if (targetSequence.length !== inputSequence.length) return false;
+	for (let i = 0; i < targetSequence.length; i++){	
 
 		if (targetSequence[i] !== inputSequence[i]) {
 		console.log("incorrect.");
-		return inPlay = false;
+		inPlay = false;
 		} else {
 		console.log("correct!");
-		return inPlay = true;
-//		getNextColor();
-		play();
+		inPlay = true;
+		inputSequence = [];
+		getNextColor();
+		}
 	}
-	}
-}
-
-
-var flashColor = function(){
-
-	for(var i=0; i<targetSequence.length; i++){
-		if (targetSequence[i] == 1){
-		// console.log("one was chosen");
-		greenLight();
-		setTimeout(greenOff, 500);
-		// setTimeout(greenLight(), 3000);
-	} else if (targetSequence[i] == 2){
-		// console.log("two was chosen");
-		redLight();
-		setTimeout(redOff, 500);
-	} else if (targetSequence[i] == 3){
-		// console.log("three was chosen");
-		yellowLight();
-		setTimeout(yellowOff, 500);
-	} else if (targetSequence[i] == 4){
-		// console.log("four was chosen");
-		blueLight();
-		setTimeout(blueOff, 500);
-	}
-
-}
 };
 
-var getNextColor = function(){
-	targetSequence.push(Math.floor((Math.random() * 4 +1)));
-	flashColor();	
-}
 
-// var play = function(){
-// 	for (var i = 0; i < 1; i++){
-// 	console.log("next is ")
-// 	getNextColor();
-// 	console.log(targetSequence);
-// 	if (targetSequence[i] == 1){
-// 		console.log("one was chosen");
-// 		greenLight();
-// 		greenOff();
-// 		// setTimeout(greenLight(), 3000);
-// 	} else if (targetSequence[i] == 2){
-// 		console.log("two was chosen");
-// 		redLight();
-// 	} else if (targetSequence[i] == 3){
-// 		console.log("three was chosen");
-// 		yellowLight();
-// 	} else if (targetSequence[i] == 4){
-// 		console.log("four was chosen");
-// 		blueLight();
-// 	}
-// 	}
-
-// }
+var flashTargetColor = function(){
+	let offset = 1000
+	for(let i=0; i<targetSequence.length; i++){
+		if (targetSequence[i] == 1){
+		// console.log("one was chosen");
+		setTimeout(greenLight, i * offset);
+		} else if (targetSequence[i] == 2){
+		// console.log("two was chosen");
+		setTimeout(redLight, i * offset);
+		} else if (targetSequence[i] == 3){
+		// console.log("three was chosen");
+		setTimeout(yellowLight, i * offset);
+		} else if (targetSequence[i] == 4){
+		// console.log("four was chosen");
+		setTimeout(blueLight, i * offset);
+		}
+	}
+};
 
 
 
@@ -123,64 +96,68 @@ var getUserChoice = function(){
 
 	var selectGreen = function(){
 	console.log("clicked on green");
+	greenLight();
 	inputSequence.push(1);
 	if (targetSequence.length === inputSequence.length){
 		compare();
 	}
-}
+	}
 
 	var selectRed = function(){
 	console.log("clicked on red");
+	redLight();
 	inputSequence.push(2);
 	if (targetSequence.length === inputSequence.length){
 		compare();
 	}
-}
+	}
 
 	var selectYellow = function(){
 	console.log("clicked on yellow");
+	yellowLight();
 	inputSequence.push(3)
 	if (targetSequence.length === inputSequence.length){
 		compare();
 	}
-}
+	}
 
 	var selectBlue = function(){
 	console.log("clicked on blue");
+	blueLight();
 	inputSequence.push(4);
 	if (targetSequence.length === inputSequence.length){
 		compare();
 	}
-}
+	}
 
-
-
-	//compare();
 
 	greenButton.on("click", selectGreen);
 	redButton.on("click", selectRed);
 	yellowButton.on("click", selectYellow);
 	blueButton.on("click", selectBlue);
 
-	// return choice;
-	// compare(choice, targetSequence);
-
-}
+};
 
 	
-
-
+var getNextColor = function(){
+	// inputSequence = [];
+	targetSequence.push(Math.floor((Math.random() * 4 +1)));
+	flashTargetColor();
+	getUserChoice();	
+};
 
 var play = function(){
+// for (var i = 0; i < 3; i++){
+	// inputSequence = [];
 	getNextColor();
 	console.log("target is " + targetSequence);
 
 	
-	getUserChoice();
+	// getUserChoice();
 
 
-}
-
+//}
+};
 
 startButton.on("click", play);
 
